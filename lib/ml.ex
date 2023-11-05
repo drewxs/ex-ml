169a7ebf2aca601eba02e376c47f2aae0a1f85a3
Andrew X. Shah <drewshah0@gmail.com>
2023-11-05 02:50:45 -0700

    [ml] rm init boilerplate

--- lib/ml.ex
+++ lib/ml.ex
@@ -2,17 +2,4 @@ defmodule Ml do
   @moduledoc """
   Documentation for `Ml`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ml.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
