7447b449771c4d0f88df301f70f5c326600c47da
Andrew X. Shah <drewshah0@gmail.com>
2023-11-05 02:52:40 -0700

    [tensor] refactor with infer_dims

--- lib/ml/tensor.ex
+++ lib/ml/tensor.ex
@@ -1,7 +1,6 @@
 defmodule Ml.Tensor do
-  alias Ml.Tensor, as: Tensor
+  alias Ml.Tensor
 
-  @spec new(dims :: [integer], data :: [number]) :: %Tensor{}
   defstruct dims: [], data: []
 
   @doc """
@@ -20,7 +19,7 @@ defmodule Ml.Tensor do
   @spec zeros(dims :: [integer]) :: %Tensor{}
   def zeros(dims) when is_list(dims) do
     data = for _ <- 1..Enum.reduce(dims, 1, &(&1 * &2)), do: 0.0
-    new(dims, data)
+    new(data)
   end
 
   @doc """
@@ -28,70 +27,92 @@ defmodule Ml.Tensor do
 
   ## Examples
 
-    iex> Tensor.new([2, 3], [[1, 2, 3], [4, 5, 6]])
+    iex> Ml.Tensor.new([2, 3], [[1, 2, 3], [4, 5, 6]])
+    %Ml.Tensor{dims: [2, 3], data: [[1, 2, 3], [4, 5, 6]]}
 
   """
-  def new(dims, data) when is_list(dims) and is_list(data) do
-    if length(dims) == 0 do
-      raise ArgumentError, message: "dims cannot be empty"
-    end
-
-    total_size = Enum.reduce(dims, 1, &(&1 * &2))
-    data_size = length(List.flatten(data))
-
-    if data_size != total_size do
-      raise ArgumentError, message: "expected #{total_size} data elements, got #{length(data)}"
-    end
-
+  @spec new(data :: [number]) :: %Tensor{}
+  def new(data) when is_list(data) do
+    {dims, _} = infer_dims(data, [])
     %Tensor{dims: dims, data: data}
   end
 
   @doc """
   Adds two tensors element-wise.
+
+  ## Examples
+
+    iex> t1 = Ml.Tensor.new([1, 3], [[1, 2, 3]])
+    iex> t2 = Ml.Tensor.new([1, 3], [[1, 1, 1]])
+    iex> Ml.Tensor.add(t1, t2)
+    %Ml.Tensor{dims: [1, 3], data: [2, 3, 4]}
+
   """
   @spec add(t1 :: %Tensor{}, t2 :: %Tensor{}) :: %Tensor{}
   def add(t1, t2) when is_tensor(t1, t2) do
     same_dims?(t1, t2)
 
-    new_dims = t1.dims
-    new_data = Enum.zip(t1.data, t2.data) |> Enum.map(fn {a, b} -> a + b end)
-    new(new_dims, new_data)
+    data = Enum.zip(t1.data, t2.data) |> Enum.map(fn {a, b} -> a + b end)
+    new(data)
   end
 
   @doc """
   Subtracts two tensors element-wise.
+
+  ## Examples
+
+    iex> t1 = Ml.Tensor.new([1, 3], [[1, 2, 3]])
+    iex> t2 = Ml.Tensor.new([1, 3], [[1, 1, 1]])
+    iex> Ml.Tensor.sub(t1, t2)
+    %Ml.Tensor{dims: [1, 3], data: [0, 1, 2]}
+
   """
   @spec sub(t1 :: %Tensor{}, t2 :: %Tensor{}) :: %Tensor{}
   def sub(t1, t2) when is_tensor(t1, t2) do
     same_dims?(t1, t2)
 
-    new_dims = t1.dims
-    new_data = Enum.zip(t1.data, t2.data) |> Enum.map(fn {a, b} -> a - b end)
-    new(new_dims, new_data)
+    data = Enum.zip(t1.data, t2.data) |> Enum.map(fn {a, b} -> a - b end)
+    new(data)
   end
 
   @doc """
   Multiplies two tensors element-wise.
+
+  ## Examples
+
+    iex> t1 = Ml.Tensor.new([1, 3], [[1, 2, 3]])
+    iex> t2 = Ml.Tensor.new([1, 3], [[2, 3, 4]])
+    iex> Ml.Tensor.mul(t1, t2)
+    %Ml.Tensor{dims: [1, 3], data: [2, 6, 12]}
+
   """
+
   @spec mul(t1 :: %Tensor{}, t2 :: %Tensor{}) :: %Tensor{}
   def mul(t1, t2) when is_tensor(t1, t2) do
     same_dims?(t1, t2)
 
-    new_dims = t1.dims
-    new_data = Enum.zip(t1.data, t2.data) |> Enum.map(fn {a, b} -> a * b end)
-    new(new_dims, new_data)
+    data = Enum.zip(t1.data, t2.data) |> Enum.map(fn {a, b} -> a * b end)
+    new(data)
   end
 
   @doc """
   Divides two tensors element-wise.
+
+  ## Examples
+
+    iex> t1 = Ml.Tensor.new([1, 3], [[4, 6, 8]])
+    iex> t2 = Ml.Tensor.new([1, 3], [[2, 3, 4]])
+    iex> Ml.Tensor.div(t1, t2)
+    %Ml.Tensor{dims: [1, 3], data: [2, 2, 2]}
+
   """
   @spec div(t1 :: %Tensor{}, t2 :: %Tensor{}) :: %Tensor{}
   def div(t1, t2) when is_tensor(t1, t2) do
     same_dims?(t1, t2)
 
-    new_dims = t1.dims
-    new_data = Enum.zip(t1.data, t2.data) |> Enum.map(fn {a, b} -> a / b end)
-    new(new_dims, new_data)
+    data = Enum.zip(t1.data, t2.data) |> Enum.map(fn {a, b} -> a / b end)
+    new(data)
+  end
 
   @doc """
   Matrix multiplies two tensors.
@@ -124,17 +145,17 @@ defmodule Ml.Tensor do
   end
 
   # Infer the dimensions of a nested list.
-  defp infer_dimensions([], dims) do
+  defp infer_dims([], dims) do
     {dims, []}
   end
 
-  defp infer_dimensions([head | tail], dims) when is_list(head) do
-    {new_dims, remaining} = infer_dimensions(head, dims)
-    infer_dimensions(tail, [length(new_dims) | new_dims] ++ remaining)
+  defp infer_dims([head | tail], dims) when is_list(head) do
+    {new_dims, remaining} = infer_dims(head, dims)
+    infer_dims(tail, [length(new_dims) | new_dims] ++ remaining)
   end
 
-  defp infer_dimensions([_ | tail], dims) do
-    infer_dimensions(tail, [1 | dims])
+  defp infer_dims([_ | tail], dims) do
+    infer_dims(tail, [1 | dims])
   end
 
   # Checks if two tensors have the same dimensions.
