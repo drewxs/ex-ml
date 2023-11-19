5673acff8fb59a5985659c47b238b880d0a3b512
Andrew X. Shah <drewshah0@gmail.com>
2023-11-19 12:41:19 -0700

    [loss] create binary cross entropy

--- lib/loss.ex
+++ lib/loss.ex
@@ -16,4 +16,33 @@ defmodule Loss do
   def mse(y_hat, y) do
     Tensor.square(Tensor.sub(y_hat, y))
   end
+
+  @doc """
+  Computes the binary cross entropy between the predicted and actual values.
+
+  ## Examples
+
+    iex> y_hat = Tensor.new([[0.9, 0.1, 0.2], [0.1, 0.9, 0.8]])
+    iex> y = Tensor.new([[1.0, 0.0, 0.0], [0.0, 1.0, 1.0]])
+    iex> Loss.bce(y_hat, y)
+    %Tensor{dims: [2, 3], data: [[0.10536051565782517, 0.10536051565782739, 0.2231435513142111],
+                                 [0.10536051565782739, 0.10536051565782517, 0.22314355131420846]]}
+
+  """
+  @spec bce(t, t) :: t
+  def bce(y_hat, y) do
+    epsilon = 1.0e-15
+    ones = Tensor.ones(y_hat.dims)
+
+    y_hat = Tensor.add(y_hat, epsilon)
+    y_hat = Tensor.clip(y_hat, epsilon, 1.0 - epsilon)
+
+    y_hat_cmp = Tensor.sub(ones, y_hat)
+    y_cmp = Tensor.sub(ones, y)
+
+    term1 = Tensor.mul(y, Tensor.ln(y_hat))
+    term2 = Tensor.mul(y_cmp, Tensor.ln(y_hat_cmp))
+
+    Tensor.mul(Tensor.add(term1, term2), -1.0)
+  end
 end
